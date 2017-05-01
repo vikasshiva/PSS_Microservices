2d2fd3dfe74ca2ab1dfe605f8a9c3be763ecdff2
Vikas Shivashankara <vikas.shivashankara@Vikass-MacBook-Pro.local>
2017-05-01 13:13:53 +0530

    adding api gateways

--- book/src/main/java/com/brownfield/pss/book/proxy/FareServiceProxy.java
+++ book/src/main/java/com/brownfield/pss/book/proxy/FareServiceProxy.java
@@ -1,7 +1,6 @@
 package com.brownfield.pss.book.proxy;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
